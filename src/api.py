055b1eb2f7a6d52a3858bf4d1e3d3448f81ed537
Mykhailo_Sindieiev <mykhailo.sindieiev@kingfisher.com>
2022-09-07 12:50:21 +0200

    Added simple implementation for getting JSON for POST method

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,22 @@ def fetch_response():
 
 @app.route('/', methods=['POST'])
 def run_scanner():
-    return jsonify({"message": 'Hello from post'})
+    # Get json from http body. Todo verify that we receiving body with application/json header, either return error
+    data = request.get_json()
+
+    source_code_url = None
+    lang = None
+    scanner_name = None
+
+    # Todo add error handling, one field is missing etc
+    if data:
+        if source_code_url in data:
+            source_code_url = data['source_code_url']
+        if lang in data:
+            lang = data['lang']
+        if scanner_name in data:
+            scanner_name = data['scanner_name']
+    return jsonify({"message": "success"})
 
 
 if __name__ == '__main__':
